15d75906d67aed70b40e0f148553cb7ed93b4833
Гиляна Бачаева <gilyanabachaeva@MacBook-Pro-Gilana.local>
2025-12-07 22:14:26 +0300

    Some changes

--- flow-manager/src/main/java/spring_micro/flow_manager/FlowManagerApplication.java
+++ flow-manager/src/main/java/spring_micro/flow_manager/FlowManagerApplication.java
@@ -8,4 +8,4 @@ public class FlowManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlowManagerApplication.class, args);
     }
-}
+}// Спокойной ночи от git! Sun Dec  7 22:10:21 MSK 2025
